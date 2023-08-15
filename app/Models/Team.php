909e100891cf2e2026e2d1c911b569f208493c02
Alemoh Rapheal <raphealenike@gmail.com>
2023-08-14 21:04:39 -0700

    Fix column error

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -2,13 +2,12 @@
 
 namespace App\Models;
 
-use App\Traits\HasUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
-    use HasFactory, HasUuid;
+    use HasFactory;
 
     public function tasks(){
         return $this->hasMany(Task::class);
--- database/migrations/2023_08_11_215813_create_teams_table.php
+++ database/migrations/2023_08_11_215813_create_teams_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->uuid("uuid");
             $table->string('name');
             $table->timestamps();
         });
